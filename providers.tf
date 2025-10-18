d15a65c74e67d0758eee5c155d436bf839ed08f0
DiazTech <cloud.dias@outlook.com>
2025-10-18 17:08:20 -0300

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  backend "s3" {
+    bucket         = "dias-state-tf"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
